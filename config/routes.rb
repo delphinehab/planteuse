960a71c83f8bd4a90dc51dad650598c19c6ea67d
Delphine Habert <delphine.habert@gmail.com>
2021-04-14 19:29:52 +0200

    navbar changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :plants, only: [:index, :show, :create]
 
-  resources :categories, only: [:index, :show, :create]
+  resources :products, only: [:index, :show, :create]
 
   resources :bookings, only: [:index, :show, :create]
 end
